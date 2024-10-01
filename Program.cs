0e887143404757de862bb939ebbc0d898212ecf8
papunagogritchiani <papuna.gogrichiani98@gmail.com>
2024-10-01 22:48:42 +0200

    week 4

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine("papuna gogritchiani");
+Console.WriteLine(Console.ReadLine());
